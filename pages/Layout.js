ab76fdb738130f25569d4042971bd257078d0d6e
Kwanele Gamedze <kwanelegamedze4@gmail.com>
2022-05-28 11:53:11 +0200

    addded loader

--- pages/Layout.js
+++ pages/Layout.js
@@ -1,9 +1,27 @@
 import Footer from "../components/Footer";
 import Nav from "../components/Nav";
+
+import styles from "../styles/layout.module.scss";
+import { useRouter } from "next/router";
+import { useEffect, useRef } from "react";
+
 export default function Layout({ children }) {
+  const loarder = useRef();
+  const router = useRouter();
+
+  useEffect(() => {
+    router.events.on("routeChangeStart", () => {
+      loarder.current.style.display = "block";
+    });
+    router.events.on("routeChangeComplete", () => {
+      loarder.current.style.display = "none";
+    });
+  }, [router]);
+
   return (
     <>
       <Nav />
+      <div ref={loarder} className={styles.loader}></div>
       <main>{children}</main>
       <Footer />
     </>
