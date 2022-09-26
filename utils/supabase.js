281d8b3b18b946b7f31fbc607c22c5af0e8a7d0f
Kwanele Gamedze <kwanelegamedze4@gmail.com>
2022-09-26 10:11:58 +0200

    everthing still works

--- utils/supabase.js
+++ utils/supabase.js
@@ -1,11 +1,11 @@
 import { SupabaseClient } from "@supabase/supabase-js";
 
 export const supabase = new SupabaseClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON
+   process.env.SUPABASE_URL,
+   process.env.SUPABASE_ANON
 );
 
 export const allTechFetcher = async () => {
-  const { data, error } = await supabase.from("tech").select();
-  return data;
+   const { data, error } = await supabase.from("tech").select();
+   return data;
 };
