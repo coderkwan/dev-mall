3d33d739108c7fc0903acca38f57ef1353f145b0
Kwanele Gamedze <kwanelegamedze4@gmail.com>
2023-04-14 18:36:33 +0200

    added creator name

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,22 +1,28 @@
 import React from "react";
 
 function Footer() {
-  return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        textAlign: "center",
-        minHeight: 100,
-        marginTop: 60,
-        paddingTop: 30,
-        borderTop: "1px solid black",
-      }}
-    >
-      Dev-mall &copy; 2022
-    </div>
-  );
+    return (
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                textAlign: "center",
+                minHeight: 100,
+                marginTop: 60,
+                paddingTop: 30,
+                borderTop: "1px solid black",
+            }}
+        >
+            <p>Dev-mall &copy; 2023</p>
+            <p>
+                Made by{" "}
+                <a href="https://kwanele.netlify.app" target="blank">
+                    Kwanele Gamedze
+                </a>
+            </p>
+        </div>
+    );
 }
 
 export default Footer;
